334ad3375c23e99eae55694655b7f68f1dcf2628
escapetherobots <escapetherobots@gmail.com>
2022-06-25 14:56:50 -0500

    Update svg and add pre-code-content

--- src/components/ui/Home.tsx
+++ src/components/ui/Home.tsx
@@ -1,14 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
     return (
         <div className="app-content">
             <div className="grid-container">
-            <div className="item1">Header</div>
-            <div className="item2">Menu</div>
-            <div className="item3">Main</div>  
-            <div className="item4">Right</div>
-            <div className="item5">Footer</div>
+            
+            {/* MENU */}
+            <div className="item2">
+                
+                <Link to="/ztest"><div className="zbtn">Code Stuff</div></Link>
+                <div className="zbtn">Illustration Stuff</div>
+            </div>
+
+            {/* MAIN  */}
+            <div className="item3">
+                
+                <img src="/assets/computer_guy_full.svg" alt="svg" />
+            </div>  
+            
+            {/* FOOTER  */}
+            <div className="item5">
+                
+            </div>
         </div>
         </div>
     );
--- src/components/ui/Ztest.tsx
+++ src/components/ui/Ztest.tsx
@@ -2,11 +2,26 @@ import React from "react";
 
 const Ztest = () => {
     // const 
+    const stuff1 = `// CODE EXAMPLE\n------------------------`;
+
+    const stuff = 
+`
+let myStuff = function() {
+    return 'something';
+}
+`;
 
     return (
         <div className="app-content">
-            
-            <img src="/assets/computer_guy_full.svg" alt="svg" />
+            <pre className="code-example">
+                <code className="pre-func">
+                    {stuff1}
+                </code>
+                <code>
+                    {stuff}
+                </code>
+            </pre>
+                            
             {/* <img src={url} /> */}
         </div>
     );
